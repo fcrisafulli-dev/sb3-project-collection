ca1030a49161c19cc47c629fb5457d7f95c969cd
Fezthedev <francescocrisafulli3@gmail.com>
2019-02-14 22:09:25 -0500

    First commit

---
+++ code/scripts/combine_json.py
@@ -0,0 +1,12 @@
+import json
+
+user="hobson-tv"
+
+json_data=[]
+for x in range (25):
+	with open("../files/%s_projects_part_%s.json" % (user,x)) as tmpjson:
+		for i in json.load(tmpjson):
+			json_data.append(i)
+
+with open("../files/%s_projects_full.json" % (user),"w") as tmpjson:
+	json.dump(json_data,tmpjson)
---
+++ code/scripts/generate_csv.py
@@ -0,0 +1,56 @@
+import json
+import csv
+
+user="hobson-tv"
+
+with open("../files/%s_projects_full.json" % (user)) as tmpjson:
+	complete_json=json.load(tmpjson)
+
+with open("../files/%s_projects.csv" %(user),"wb+") as tmpcsv:
+	cw = csv.writer(tmpcsv)
+	cw.writerow([
+		"title",
+		"instructions",
+		"description",
+		"author_id",
+		"id",
+		"stats_views",
+		"stats_loves",
+		"stats_favorites",
+		"stats_comments",
+		"author_scratchteam",
+		"comments_allowed",
+		"is_published",
+		"public",
+		"visibility",
+		"author_history_joined",
+		"history_created",
+		"history_shared",
+		"history_modified",
+		"stats_remixes",
+		"remix_root",
+		"remix_parent"])
+	for project in complete_json:
+		cw.writerow([
+			project["title"].encode("utf-8").strip(),
+			project["instructions"].encode("utf-8").strip(),
+			project["description"].encode("utf-8").strip(),
+			project["author"]["id"],
+			project["id"],
+			project["stats"]["views"],
+			project["stats"]["loves"],
+			project["stats"]["favorites"],
+			project["stats"]["comments"],
+			project["author"]["scratchteam"],
+			project["comments_allowed"],
+			project["is_published"],
+			project["public"],
+			project["visibility"],
+			project["author"]["history"]["joined"],
+			project["history"]["created"],
+			project["history"]["shared"],
+			project["history"]["modified"],
+			project["stats"]["remixes"],
+			project["remix"]["root"],
+			project["remix"]["parent"]
+			])
